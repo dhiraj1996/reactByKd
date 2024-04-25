e39b68d6d79a43fc2727fd52f87a59503b63daee
dhiraj1996 <dhirajkumar1996.com>
2024-04-25 23:17:37 +0530

    more changes

--- 37-social-media-initial/src/components/Post.jsx
+++ 37-social-media-initial/src/components/Post.jsx
@@ -1,25 +1,44 @@
 // import React from 'react'
 import { TbHttpDelete } from "react-icons/tb";
 import { AiOutlineLike } from "react-icons/ai";
+import { useContext } from "react";
+import { PostList } from "../store/post-list-store";
 
 
 function Post({ post }) {
+  
+  const {deletePost} = useContext(PostList);
+
   return (
     <div
       className="card d-inline-flex p-2 post-card"
-      style={{ width: "18rem" }}
+      style={{ width: "18rem", height: "280px" }}
     >
       <div className="card-body">
         <h5 className="card-title">{post.title}</h5>
         <p className="card-text">{post.body}</p>
+
         {post.tags.map((tag) => (
           <span key={tag} className="badge text-bg-dark tag-but">
             {tag}
           </span>
         ))}
-        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-        {post.reactions}
-        </span>
+        
+        <button
+          className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger btn"
+          onClick={() => deletePost(post.id)}
+        >
+          <TbHttpDelete />
+        </button>
+        <div>
+        <div className="alert alert-danger" role="alert">
+        Total reaction in this post      
+            <span className="badge text-bg-success">
+              <AiOutlineLike />
+              {post.reactions}
+            </span>
+        </div>
+        </div>
       </div>
     </div>
   );
