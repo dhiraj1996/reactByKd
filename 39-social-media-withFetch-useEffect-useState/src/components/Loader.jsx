6f457f75ad9abaa9fa1593de49fa1fa50bf89eab
dhiraj1996 <dhirajkumar1996.com>
2024-05-22 20:53:36 +0530

    fetch using useEffect and commented useState

---
+++ 39-social-media-withFetch-useEffect-useState/src/components/Loader.jsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+    return(
+        <div className="d-flex justify-content-center loader">
+            <div className="spinner-border" style={{width: "6rem",height: "6rem"}} role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    );
+}
+
+export default Loader;
